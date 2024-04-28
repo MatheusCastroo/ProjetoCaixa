4a98f3d5630c85fef74e8fcc6cfb58a2d034b693
Matheus Castro <113482506+MatheusCastroo@users.noreply.github.com>
2024-04-28 09:02:12 -0300

    Inserindo mais itens

--- teste.php
+++ teste.php
@@ -1,47 +1,66 @@
 <?php
-$item = array (
+
+$itens = array (
+    array (
     "nome"=> "Rack",
-    "comprimento" => 1,
-    "largura" => 1,
-    "altura" => 30,
+    "comprimento" => 28,
+    "largura" => 26,
+    "altura" => 34,
     "peso" => 7,
-    "quantidade" => 2  
+    "quantidade" => 2
+    ),
+    array (
+        "nome"=> "Rack",
+        "comprimento" => 20,
+        "largura" => 30,
+        "altura" => 31,
+        "peso" => 7,
+        "quantidade" => 1
+        ),
+        array (
+            "nome"=> "Rack",
+            "comprimento" => 25,
+            "largura" => 15,
+            "altura" => 32,
+            "peso" => 7,
+            "quantidade" => 4
+        ),
 );
+$total_volume = 0;
+
+foreach ($itens as $item) {
+    $volume_item = $item["comprimento"] * $item["largura"] * $item["altura"] * $item["quantidade"];
+    $total_volume += $volume_item;
+}
 
 $caixas = array (
-    "caixa P" => array (
+    "P" => array (
         "comprimento" => 28,
         "largura" => 27,
         "altura" => 35
     ),
-    "caixa M" => array (
+    "M" => array (
         "comprimento" => 38,
         "largura" => 24,
         "altura" => 56
     ),
-    "caixa G" => array (
+    "G" => array (
         "comprimento" => 41,
         "largura" => 35,
         "altura" => 61
     )
 );
 
-$item["comprimento"] *= $item["quantidade"];
-$item["largura"] *= $item["quantidade"];
-$item["altura"] *= $item["quantidade"];
-
 $encaixou = false;
-foreach ($caixas as $chave => $value) {
-    if ($item["comprimento"] <= $value["comprimento"] && $item["largura"] <= $value["largura"] && $item["altura"] <= $value["altura"]) {
-        echo "O ". $item["nome"]. " vai para ". $chave;
+foreach ($caixas as $chave => $caixa) {   
+    if ($total_volume <= $caixa["comprimento"] * $caixa["largura"] * $caixa["altura"]) {
+        echo "Os itens vai para caixa ". $chave;
         $encaixou = true;
         break;
     }
 }
+
 if (!$encaixou) {
     echo "Joga a mercadoria no caminhão sem caixa mesmo.";
 }
 ?>
-<form method="post" action="index.php">
-    
-<input type="submit" name="voltar" value="Voltar">
