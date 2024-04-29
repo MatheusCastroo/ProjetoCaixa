b21ba96cba618652a3cdf4d60adc76257da933fd
Matheus Castro <113482506+MatheusCastroo@users.noreply.github.com>
2024-04-29 07:58:36 -0300

    Update teste.php

--- teste.php
+++ teste.php
@@ -1,47 +1,102 @@
 <?php
-$item = array (
-    "nome"=> "Rack",
-    "comprimento" => 1,
-    "largura" => 1,
-    "altura" => 30,
-    "peso" => 7,
-    "quantidade" => 2  
-);
 
-$caixas = array (
-    "caixa P" => array (
+$itens = array(
+    array(
+        "nome" => "Rack",
+        "comprimento" => 28,
+        "largura" => 26,
+        "altura" => 34,
+        "peso" => 7,
+        "quantidade" => 2
+    ),
+    array(
+        "nome" => "Rack",
+        "comprimento" => 20,
+        "largura" => 30,
+        "altura" => 31,
+        "peso" => 7,
+        "quantidade" => 1
+    ),
+    array(
+        "nome" => "Rack",
+        "comprimento" => 25,
+        "largura" => 15,
+        "altura" => 32,
+        "peso" => 7,
+        "quantidade" => 4
+    ),
+);
+//Calculo da cubagem
+$total_volume = 0;
+
+foreach ($itens as $item) {
+    $volume_item = $item["comprimento"] * $item["largura"] * $item["altura"] * $item["quantidade"];
+    $total_volume += $volume_item;
+}
+
+$caixas = array(
+    "P" => array(
         "comprimento" => 28,
         "largura" => 27,
         "altura" => 35
     ),
-    "caixa M" => array (
+    "M" => array(
         "comprimento" => 38,
         "largura" => 24,
         "altura" => 56
     ),
-    "caixa G" => array (
+    "G" => array(
         "comprimento" => 41,
         "largura" => 35,
         "altura" => 61
     )
 );
 
-$item["comprimento"] *= $item["quantidade"];
-$item["largura"] *= $item["quantidade"];
-$item["altura"] *= $item["quantidade"];
-
 $encaixou = false;
-foreach ($caixas as $chave => $value) {
-    if ($item["comprimento"] <= $value["comprimento"] && $item["largura"] <= $value["largura"] && $item["altura"] <= $value["altura"]) {
-        echo "O ". $item["nome"]. " vai para ". $chave;
+
+// Verifica se os itens cabem em uma única caixa
+foreach ($caixas as $chave => $caixa) {
+    if ($total_volume <= $caixa["comprimento"] * $caixa["largura"] * $caixa["altura"]) {
+        echo "Os itens vão para caixa " . $chave;
         $encaixou = true;
         break;
     }
 }
+
+// Se não couber em uma única caixa, tenta combinações de caixas
 if (!$encaixou) {
-    echo "Joga a mercadoria no caminhão sem caixa mesmo.";
+    $combinacoes = array(
+        array("P", "P"),
+        array("P", "M"),
+        array("M", "M"),
+        array("M", "G"),
+        array("G", "G"),
+        array("P", "M", "G"),
+        array("P", "P", "M"),
+        array("P", "P", "G"),
+        array("P", "M", "M"),
+        array("M", "M", "G")
+    );
+
+    foreach ($combinacoes as $comb) {
+        $volume_combinado = 0;
+        foreach ($comb as $tamanho) {
+            $volume_combinado += $caixas[$tamanho]["comprimento"] * $caixas[$tamanho]["largura"] * $caixas[$tamanho]["altura"];
+        }
+        if ($total_volume <= $volume_combinado) {
+            echo "Os itens vão para caixas: " . implode(", ", $comb);
+            $encaixou = true;
+            break;
+        }
+    }
+
+    if (!$encaixou) {
+        echo "Jogue a mercadoria no caminhão sem caixa mesmo.";
+    }
 }
-?>
-<form method="post" action="index.php">
-    
-<input type="submit" name="voltar" value="Voltar">
+//Incluir o peso em kg para cada retorno.
+
+//Mostrar quais produtos foi para quais caixas, incluindo a quantidade dos produtos em cada caixa.
+
+/*Caso não houver nenhuma caixa para algum produto especifico, incluir os outros itens na caixa e validar 
+o item que não é possivel inserir em caixa.*/
