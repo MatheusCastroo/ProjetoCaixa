f3d1f3e25b3a8e387ef9f04ef8144d611b0ebfce
Matheus Castro <113482506+MatheusCastroo@users.noreply.github.com>
2024-05-05 12:29:23 -0300

    Validar caixas e peso

--- teste.php
+++ teste.php
@@ -1,98 +1,100 @@
 <?php
-
 $itens = array(
     array(
-        "nome" => "Rack",
+        "nome" => "Primeiro Rack",
         "comprimento" => 28,
-        "largura" => 26,
-        "altura" => 34,
+        "largura" => 15,
+        "altura" => 30,
         "peso" => 7,
-        "quantidade" => 2
+        "quantidade" => 5
     ),
     array(
-        "nome" => "Rack",
-        "comprimento" => 20,
+        "nome" => "Segundo Rack",
+        "comprimento" => 35,
         "largura" => 30,
         "altura" => 31,
         "peso" => 7,
-        "quantidade" => 1
+        "quantidade" => 10
     ),
     array(
-        "nome" => "Rack",
-        "comprimento" => 25,
-        "largura" => 15,
-        "altura" => 32,
+        "nome" => "Terceiro Rack",
+        "comprimento" => 20,
+        "largura" => 20,
+        "altura" => 30,
         "peso" => 7,
-        "quantidade" => 4
+        "quantidade" => 200
     ),
 );
-
 $total_volume = 0;
+$total_peso = 0;
 
+//Calculo de cubagem
 foreach ($itens as $item) {
     $volume_item = $item["comprimento"] * $item["largura"] * $item["altura"] * $item["quantidade"];
     $total_volume += $volume_item;
+    $total_peso += $item["peso"] * $item["quantidade"];
 }
 
 $caixas = array(
     "P" => array(
         "comprimento" => 28,
         "largura" => 27,
-        "altura" => 35
+        "altura" => 35,
+        "peso_maximo" => 20
     ),
     "M" => array(
         "comprimento" => 38,
         "largura" => 24,
-        "altura" => 56
+        "altura" => 56,
+        "peso_maximo" => 30
     ),
     "G" => array(
         "comprimento" => 41,
         "largura" => 35,
-        "altura" => 61
+        "altura" => 61,
+        "peso_maximo" => 40
     )
 );
 
 $encaixou = false;
+$itens_caixa = array(); //Armazenar os itens
 
-
-foreach ($caixas as $chave => $caixa) {
-    if ($total_volume <= $caixa["comprimento"] * $caixa["largura"] * $caixa["altura"]) {
-        echo "Os itens vão para caixa " . $chave;
-        $encaixou = true;
-        break;
-    }
-}
-
-
-if (!$encaixou) {
-    $combinacoes = array(
-        array("P", "G"),
-        array("M", "M"),
-        array("M", "G"),
-        array("G", "G"),
-        array("M", "M", "G"),
-        array("M", "G", "G"),
-        array("G", "G", "G")   
-    );
-    foreach ($combinacoes as $comb) {
-        $volume_combinado = 0;
-        foreach ($comb as $tamanho) {
-            $volume_combinado += $caixas[$tamanho]["comprimento"] * $caixas[$tamanho]["largura"] * $caixas[$tamanho]["altura"];
-        }
-        if ($total_volume <= $volume_combinado) {
-            echo "Os itens vão para caixas: " . implode(", ", $comb);
-            $encaixou = true;
+foreach ($itens as $item) {
+    $item_encaixado = false;
+    foreach ($caixas as $chave => $caixa) {
+        if (
+            $item["comprimento"] <= $caixa["comprimento"] &&
+            $item["largura"] <= $caixa["largura"] &&
+            $item["altura"] <= $caixa["altura"]
+        ) {
+            //Caso couber na caixa, incluir na lista de itens para essa caixa.
+            $itens_caixa[$chave][] = $item;
+            $item_encaixado = true;
             break;
         }
     }
-
-    if (!$encaixou) {
-        echo "Jogue a mercadoria no caminhão sem caixa mesmo.";
-    }
+    //Caso não couber, verificar:
+        if (!$item_encaixado) {
+            echo "O item ". $item['nome']. "com dimensões ". $item['comprimento']."x".$item['largura']."x".$item['altura']. "não pode ser encaixado em nenhuma caixa. <br>";
+        }
 }
-//Incluir o peso em kg para cada retorno.
 
-//Mostrar quais produtos foi para quais caixas, incluindo a quantidade dos produtos em cada caixa.
+//Itens inserido em cada caixa
+foreach ($itens_caixa as $chave => $itens_na_caixa) {
+    echo "<p> Itens na caixa $chave: </p><ul>";
+    foreach ($itens_na_caixa as $item) {
+        echo "<li>" .$item['quantidade']. " unidades do item: " .$item['nome']. " </li>";
+    }
+    //Peso total
+    $peso_caixa = 0;
+    foreach ($itens_na_caixa as $item) {
+    $peso_caixa += $item["peso"] * $item["quantidade"];
+    }
+    echo "</ul><p>Peso total: $peso_caixa kg.</p><br>";
+    $encaixou = true;
+}
 
-/*Caso não houver nenhuma caixa para algum produto especifico, incluir os outros itens na caixa e validar 
-o item que não é possivel inserir em caixa.*/
+if (!$encaixou) {
+    echo "ERRO, Verifique os dados!";
+}
+?>
