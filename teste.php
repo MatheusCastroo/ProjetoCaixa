91781de662ac0a968f960a91316a09ad635ddfdb
Matheus Castro <113482506+MatheusCastroo@users.noreply.github.com>
2024-05-28 20:26:00 -0300

    Update teste.php

--- teste.php
+++ teste.php
@@ -14,7 +14,7 @@ $itens = array(
         "largura" => 15,
         "altura" => 30,
         "peso" => 7,
-        "quantidade" => 26,
+        "quantidade" => 3,
     ),
     array(
         "nome" => "Terceiro Item",
@@ -25,16 +25,6 @@ $itens = array(
         "quantidade" => 8,
     ),
 );
-
-$total_volume = 0;
-$total_peso = 0;
-
-// Calculo de cubagem
-foreach ($itens as $item) {
-    $volume_item = $item["comprimento"] * $item["largura"] * $item["altura"] * $item["quantidade"];
-    $total_volume += $volume_item;
-    $total_peso += $item["peso"] * $item["quantidade"];
-}
 
 $caixas = array(
     "P" => array(
@@ -60,6 +50,10 @@ $caixas = array(
 $itens_caixa = array(); // Armazenar os itens
 $caixas_utilizadas = array("P" => 0, "M" => 0, "G" => 0); // Contador de caixas utilizadas
 
+function calcula_volume($comprimento, $largura, $altura) {
+    return $comprimento * $largura * $altura;
+}
+
 foreach ($itens as $item) {
     $quantidade_restante = $item["quantidade"];
     
@@ -73,57 +67,45 @@ foreach ($itens as $item) {
                 $itens_caixa[$caixa_id] = array(
                     "tipo" => $chave,
                     "itens" => array(),
-                    "peso_atual" => 0
+                    "peso_atual" => 0,
+                    "volume_atual" => 0
                 );
             }
+
+            $volume_item = calcula_volume($item["comprimento"], $item["largura"], $item["altura"]);
+            $volume_caixa = calcula_volume($caixa["comprimento"], $caixa["largura"], $caixa["altura"]);
             
+            // Verifica se o item cabe na caixa e se não ultrapassa o peso máximo ou volume máximo
             if (
-                ($item["comprimento"] <= $caixa["comprimento"] &&
-                 $item["largura"] <= $caixa["largura"] &&
-                 $item["altura"] <= $caixa["altura"]) ||
-           
-                ($item["comprimento"] <= $caixa["comprimento"] &&
-                 $item["altura"] <= $caixa["largura"] &&
-                 $item["largura"] <= $caixa["altura"]) ||
-           
-                ($item["largura"] <= $caixa["comprimento"] &&
-                 $item["comprimento"] <= $caixa["largura"] &&
-                 $item["altura"] <= $caixa["altura"]) ||
-           
-                ($item["largura"] <= $caixa["comprimento"] &&
-                 $item["altura"] <= $caixa["largura"] &&
-                 $item["comprimento"] <= $caixa["altura"]) ||
-           
-                ($item["altura"] <= $caixa["comprimento"] &&
-                 $item["comprimento"] <= $caixa["largura"] &&
-                 $item["largura"] <= $caixa["altura"]) ||
-           
-                ($item["altura"] <= $caixa["comprimento"] &&
-                 $item["largura"] <= $caixa["largura"] &&
-                 $item["comprimento"] <= $caixa["altura"])
+                $volume_item <= $volume_caixa &&
+                $itens_caixa[$caixa_id]["peso_atual"] + $item["peso"] <= $caixa["pesoMaximo"] &&
+                $itens_caixa[$caixa_id]["volume_atual"] + $volume_item <= $volume_caixa
             ) {
-                // Verifica se o peso total não ultrapassa o peso máximo da caixa
-                if ($itens_caixa[$caixa_id]["peso_atual"] + $item["peso"] <= $caixa["pesoMaximo"]) {
-                    // Caso couber na caixa, incluir na lista de itens para essa caixa.
-                    $itens_caixa[$caixa_id]["itens"][] = array(
-                        "nome" => $item["nome"],
-                        "comprimento" => $item["comprimento"],
-                        "largura" => $item["largura"],
-                        "altura" => $item["altura"],
-                        "peso" => $item["peso"],
-                        "quantidade" => 1 // Adiciona uma unidade por vez
-                    );
-                    $itens_caixa[$caixa_id]["peso_atual"] += $item["peso"];
-                    $quantidade_restante--;
-                    $item_encaixado = true;
-                    break;
-                }
+                // Caso couber na caixa, incluir na lista de itens para essa caixa.
+                $itens_caixa[$caixa_id]["itens"][] = array(
+                    "nome" => $item["nome"],
+                    "comprimento" => $item["comprimento"],
+                    "largura" => $item["largura"],
+                    "altura" => $item["altura"],
+                    "peso" => $item["peso"],
+                    "quantidade" => 1 // Adiciona uma unidade por vez
+                );
+                $itens_caixa[$caixa_id]["peso_atual"] += $item["peso"];
+                $itens_caixa[$caixa_id]["volume_atual"] += $volume_item;
+                $quantidade_restante--;
+                $item_encaixado = true;
+                break;
             }
         }
         
         // Caso não couber em nenhuma caixa atual, criar uma nova caixa do mesmo tipo
         if (!$item_encaixado) {
-            $caixas_utilizadas[$chave]++;
+            foreach ($caixas as $chave => $caixa) {
+                if ($caixa_id == $chave . "_" . $caixas_utilizadas[$chave]) {
+                    $caixas_utilizadas[$chave]++;
+                    break;
+                }
+            }
         }
     }
 }
